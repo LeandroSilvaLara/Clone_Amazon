87de6f77b657d91df0d0907d7c0afa97638cca28
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2020-12-22 22:29:47 -0300

    adiconado imagens no FrontEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
@@ -14,13 +15,18 @@ function App() {
     </header>
     <main>
         <div className="row center">
-            <div className="card">
-                <a href="product.html">
-                    <img className="medium " src="./images/p1.jpg" alt="product" />
+        {data.products.map((product =>
+            <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                    <img 
+                    className="medium" 
+                    src={product.image} 
+                    alt={product.name}
+                    />
                 </a>
                 <div className="card-body">
-                    <a href="product.html">
-                        <h2>Nike Slim Shirts</h2>
+                    <a href={`/product/${product._id}`}>
+                        <h2>{product.name}</h2>
                     </a>
                     <div className="rating">
                         <span> <i className="fa fa-star"></i> </span>
@@ -31,16 +37,15 @@ function App() {
                         <span> <i className="fa fa-star"></i> </span>
                         <span> <i className="fa fa-star"></i> </span>
                     </div>
-                    <div className="price">
-                        $120
-                    </div>
+                    <div className="price">${product.price}</div>
                 </div>
             </div>
+        ))}
         </div>
     </main>
     <footer className="row center">All right reserved</footer>
 </div>
-  );
+);
 }
 
 export default App;
